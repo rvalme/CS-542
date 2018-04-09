59371e762aa6d6993ecfdf07ca6d1d74b04f2be1
metamatea <31784686+metamatea@users.noreply.github.com>
2018-04-08 23:27:27 -0400

    Add files via upload

--- server/server_interface.py
+++ server/server_interface.py
@@ -10,6 +10,7 @@ import sys
 from server.query_factory import QueryFactory as query_factory
 from server.dao_recipe import DaoRecipe as dao_recipe
 from server.sort_by import SortBy as sort
+from server.limitation_choice import limitchoice
 
 class Singleton(object):
     '''
@@ -139,85 +140,110 @@ class ServerInterface(Singleton):
         self.printf("Error code = %s\n", error.code)
         self.printf("Error message = %s\n", error.message)
 
-'''
-Sort recipes by fat/calories/sugar/protein
-'''
 
-def sort_by_nutrient(self,choice=0,nutrichoice=0):
-    recipe_fat = []
-    recipe_calories = []
-    recipe_sugar = []
-    recipe_protein = []
-    try:
-        connection = cx_Oracle.connect('cwang9', 'CWANG9', cx_Oracle.makedsn('oracle.wpi.edu', 1521, 'ORCL'));
-    except:
-        print('Error: Could not connect to database')
-    else:
-        print('Connected to Oracle successfully')
-        cur = connection.cursor()
-        if choice == 1:
-            if nutrichoice == 1:
-                cur.execute(sort.sort_calories_vegan())
-                for result in cur:
-                    recipe_calories.append(result)
-                return recipe_calories
-            elif nutrichoice == 2:
-                cur.execute(sort.sort_fat_vegan())
-                for result in cur:
-                    recipe_fat.append(result)
-                return recipe_fat
-            elif nutrichoice == 3:
-                cur.execute(sort.sort_sugar_vegan())
-                for result in cur:
-                    recipe_sugar.append(result)
-                return recipe_sugar
-            elif nutrichoice == 4:
-                cur.execute(sort.sort_protein_vegan())
-                for result in cur:
-                    recipe_sugar.append(result)
-                return recipe_protein
-        elif choice == 2:
-            if nutrichoice == 1:
-                cur.execute(sort.sort_calories_vegetarian())
-                for result in cur:
-                    recipe_calories.append(result)
-                return recipe_calories
-            elif nutrichoice == 2:
-                cur.execute(sort.sort_fat_vegetarian())
-                for result in cur:
-                    recipe_fat.append(result)
-                return recipe_fat
-            elif nutrichoice == 3:
-                cur.execute(sort.sort_sugar_vegetarian())
-                for result in cur:
-                    recipe_sugar.append(result)
-                return recipe_sugar
-            elif nutrichoice == 4:
-                cur.execute(sort.sort_protein_vegetarian())
-                for result in cur:
-                    recipe_sugar.append(result)
-                return recipe_protein
-        elif choice == 3:
-            if nutrichoice == 1:
-                cur.execute(sort.sort_calories_paleo())
-                for result in cur:
-                    recipe_calories.append(result)
-                return recipe_calories
-            elif nutrichoice == 2:
-                cur.execute(sort.sort_fat_paleo())
-                for result in cur:
-                    recipe_fat.append(result)
-                return recipe_fat
-            elif nutrichoice == 3:
-                cur.execute(sort.sort_sugar_paleo())
-                for result in cur:
-                    recipe_sugar.append(result)
-                return recipe_sugar
-            elif nutrichoice == 4:
-                cur.execute(sort.sort_protein_paleo())
-                for result in cur:
-                    recipe_sugar.append(result)
-                return recipe_protein
+    def sort_by_nutrient(self,choice=0,nutrichoice=0):
+        recipe_fat = []
+        recipe_calories = []
+        recipe_sugar = []
+        recipe_protein = []
+        try:
+            connection = cx_Oracle.connect('cwang9', 'CWANG9', cx_Oracle.makedsn('oracle.wpi.edu', 1521, 'ORCL'));
+        except:
+            print('Error: Could not connect to database')
+        else:
+            print('Connected to Oracle successfully')
+            cur = connection.cursor()
+            if choice == 1:
+                if nutrichoice == 1:
+                    cur.execute(sort.sort_calories_vegan())
+                    for result in cur:
+                        recipe_calories.append(result)
+                    return recipe_calories
+                elif nutrichoice == 2:
+                    cur.execute(sort.sort_fat_vegan())
+                    for result in cur:
+                        recipe_fat.append(result)
+                    return recipe_fat
+                elif nutrichoice == 3:
+                    cur.execute(sort.sort_sugar_vegan())
+                    for result in cur:
+                        recipe_sugar.append(result)
+                    return recipe_sugar
+                elif nutrichoice == 4:
+                    cur.execute(sort.sort_protein_vegan())
+                    for result in cur:
+                        recipe_sugar.append(result)
+                    return recipe_protein
+            elif choice == 2:
+                if nutrichoice == 1:
+                    cur.execute(sort.sort_calories_vegetarian())
+                    for result in cur:
+                        recipe_calories.append(result)
+                    return recipe_calories
+                elif nutrichoice == 2:
+                    cur.execute(sort.sort_fat_vegetarian())
+                    for result in cur:
+                        recipe_fat.append(result)
+                    return recipe_fat
+                elif nutrichoice == 3:
+                    cur.execute(sort.sort_sugar_vegetarian())
+                    for result in cur:
+                        recipe_sugar.append(result)
+                    return recipe_sugar
+                elif nutrichoice == 4:
+                    cur.execute(sort.sort_protein_vegetarian())
+                    for result in cur:
+                        recipe_sugar.append(result)
+                    return recipe_protein
+            elif choice == 3:
+                if nutrichoice == 1:
+                    cur.execute(sort.sort_calories_paleo())
+                    for result in cur:
+                        recipe_calories.append(result)
+                    return recipe_calories
+                elif nutrichoice == 2:
+                    cur.execute(sort.sort_fat_paleo())
+                    for result in cur:
+                        recipe_fat.append(result)
+                    return recipe_fat
+                elif nutrichoice == 3:
+                    cur.execute(sort.sort_sugar_paleo())
+                    for result in cur:
+                        recipe_sugar.append(result)
+                    return recipe_sugar
+                elif nutrichoice == 4:
+                    cur.execute(sort.sort_protein_paleo())
+                    for result in cur:
+                        recipe_sugar.append(result)
+                    return recipe_protein
+
+    def select_exclude(self,dtype, ingred):
+        exclude = []
+        try:
+            connection = cx_Oracle.connect('cwang9', 'CWANG9', cx_Oracle.makedsn('oracle.wpi.edu', 1521, 'ORCL'));
+        except:
+            print('Error: Could not connect to database')
+        else:
+            print('Connected to Oracle successfully')
+            cur = connection.cursor()
+            cur.execute(limitchoice.exclude_ingredient(dtype,ingred))
+            for result in cur:
+                exclude.append(result)
+        return exclude
+
+    def select_limitation(self,dtype, quant):
+        limit = []
+        try:
+            connection = cx_Oracle.connect('cwang9', 'CWANG9', cx_Oracle.makedsn('oracle.wpi.edu', 1521, 'ORCL'));
+        except:
+            print('Error: Could not connect to database')
+        else:
+            print('Connected to Oracle successfully')
+            cur = connection.cursor()
+            cur.execute(limitchoice.nutrient_limit(dtype,quant))
+            for result in cur:
+                limit.append(result)
+        return limit
 
 
 
